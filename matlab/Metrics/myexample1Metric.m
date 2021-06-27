b0a0f93426e15c7a11319d772dfabc0994e3497b
sefsnf <ssneff@ucsc.edu>
2021-06-26 20:30:25 -0700

    geodesic, geoStep
    crudely implemented geodesic and forward euler geostep using isInside (basically copy/pasted from geodesics.m)
    
    everything seems to be working as intended
    
    need to more properly handle variables like h
    need to go thru and unify naming convention between method variables

--- matlab/Metrics/myexample1Metric.m
+++ matlab/Metrics/myexample1Metric.m
@@ -1,11 +1,8 @@
 classdef myexample1Metric < Metric
     methods
         function out = lg(obj,x,y)
-            out = square(x) + square(y);
+            %out = 5 * exp(-1./(1-min(abs(x),1).^2));
+            out = -2 + 2 * exp(-1./(1-min(max(x*5,0),1).^2));
         end
     end
-end
-
-function out = square(X)
-    out = 2 * (sin(X) > 0) - 1;
 end
--- matlab/RiemannSurface.m
+++ matlab/RiemannSurface.m
@@ -21,6 +21,93 @@ classdef RiemannSurface
         end
         
         
+        function [xO,yO,thO] = geodesic(obj,xI,yI,thI)
+            met = obj.metric;
+            dom = obj.domain;
+            
+            h = 0.01;
+            NMAX = floor(100/h);
+
+            % initialize (x,y,th)
+
+            ngeo = length(xI);
+
+            xO = zeros(ngeo, NMAX); xO(:,1) = xI;
+            yO = zeros(ngeo, NMAX); yO(:,1) = yI;
+            thO = zeros(ngeo,NMAX); thO(:,1) = thI;
+            
+            t = 1;
+            insidepoints = dom.isInside(xI,yI);
+
+            while any(insidepoints) && (t < NMAX)
+                IPidx = find(insidepoints);
+                noIPidx = find(~insidepoints);
+
+                % move the inside points forward
+                [xO(IPidx,t+1), yO(IPidx,t+1), thO(IPidx,t+1)] =  ...
+                    obj.geoStep(xO(IPidx,t), yO(IPidx,t), thO(IPidx,t));
+
+                % keep everything fixed for points that reached the boundary
+                xO(noIPidx, t+1) = xO(noIPidx, t);
+                yO(noIPidx, t+1) = yO(noIPidx, t);
+                thO(noIPidx, t+1) = thO(noIPidx, t);
+
+                % update inside points and march time forward
+                insidepoints(IPidx) = dom.isInside(xO(IPidx, t+1),yO(IPidx, t+1));
+                t = t+1;    
+
+                % debug visualisation
+            %     clf    
+            %     plot(exp(1i*[0:2*pi/100:2*pi, 0]), 'b'); hold on; axis equal
+            %     plot(x(:,1:t)',y(:,1:t)','r'); 
+            %     pause
+
+            end
+            
+            xO = xO(:, 1:t-1); yO = yO(:, 1:t-1); 
+            thO = thO(:, 1:t-1); 
+            
+            xO = xO';
+            yO = yO';
+            thO = thO';
+        end    
+        
+        function plotGeoRadiate(obj, x,y)
+            holdBool = ishold;
+            hold on;
+            obj.plot()
+            
+            
+            thI = linspace(0,2*pi, 80);
+            xI = ones(size(thI)) * x;
+            yI = ones(size(thI)) * y;
+            
+            [xO,yO,~] = obj.geodesic(xI,yI,thI);
+            plot(xO,yO,'r')
+            plot(x,y,'r*')
+            
+            if (~holdBool), hold off; end;
+        end    
+        
+        
+        function plotGeoParallels(obj, x,y,th)
+            holdBool = ishold;
+            hold on;
+            obj.plot()
+            
+            
+            off = linspace(-4,4, 80);
+            xI = -sin(th) * off + x;
+            yI = cos(th) * off + y;
+            thI = ones(size(off)) * th;
+            
+            [xO,yO,~] = obj.geodesic(xI,yI,thI);
+            plot(xO,yO,'r')
+            plot(x,y,'r*')
+            plot(xI,yI,'r*','MarkerSize',1)
+            
+            if (~holdBool), hold off; end;
+        end  
         
         function plot(obj) % make nicer?
             holdBool = ishold;
@@ -51,6 +138,22 @@ classdef RiemannSurface
         
         
     end
+    
+    methods (Access = 'protected')
+        function [xO,yO,thO] = geoStep(obj,xI,yI,thI)
+            h = 0.01;
+            
+            met = obj.metric;
+        	[lg,dxlg,dylg] = met.metricVals(xI, yI);
+            cth = cos(thI); sth = sin(thI);
+
+            hh = exp(-0.5*lg)*h;
+
+            xO = xI + hh.*cth;
+            yO = yI + hh.*sth;
+            thO = thI + .5*hh.*(cth.*dylg - sth.*dxlg);
+        end    
+    end    
 end
 
 
